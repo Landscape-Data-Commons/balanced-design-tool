e6d4699919152fb2188ef94acd6e8ba89d2c9fa6
nstauffer <ngstauffer@gmail.com>
2019-07-01 10:43:56 -0700

    Give busy notifications a proper A E S T H E T I C

--- ui.R
+++ ui.R
@@ -1,6 +1,48 @@
 library(shiny)
 
 fluidPage(
+  # This changes the position of the notification
+  tags$head(
+    tags$style(
+      HTML(
+        ".shiny-notification {
+          position:fixed;
+          bottom: calc(60%);
+          left: calc(5%);
+          width: calc(25%);
+          opacity: 1;
+          font-weight: bold;
+          box-shadow: 0 0 0 rgba(204,169,44, 0.4);
+          animation: pulse 2s infinite;
+        }
+        @-webkit-keyframes pulse {
+          0% {
+            -webkit-box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+          }
+          70% {
+            -webkit-box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+          }
+          100% {
+            -webkit-box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+          }
+        }
+        @keyframes pulse {
+          0% {
+            -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+            box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+          }
+          70% {
+            -moz-box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+            box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+          }
+          100% {
+            -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+            box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+          }
+        }"
+      )
+    )
+  ),
   titlePanel("Spatially Balanced Sampling Tool"),
   sidebarLayout(
     sidebarPanel(
