aece5364e24cf41248653185a07de3a913a40acf
nstauffer <ngstauffer@gmail.com>
2019-07-17 14:41:27 -0700

    Restructure and streamline output script

--- draw_pt1.R
+++ draw_pt1.R
@@ -3,8 +3,14 @@
 ## When run, this script will reproduce the sample points it was packaged with using a
 ## Generalized Random Tessellation Stratified approach.
 
-## The only user input necessary is to make sure that the packages in section 2 are installed
+## The only user input necessary is to make sure that the following packages are installed
 ## and to make sure that the values for input.filepath and output.filepath in section 1 are correct.
+
+## If any of these are not installed, remove the hashes and run the relevant line:
+# install.packages("dplyr")
+# install.packages("rgdal")
+# install.packages("spsurvey")
+# install.packages("stringr")
 
 #------------------------------------------------------------------------------------#
 ## The contents are as follows:
@@ -13,17 +19,14 @@
 # This section contains archival information about how the the design was configured (which should NOT be edited)
 # and the input.filepath and output.filepath values (which MUST be edited).
 
-#-- Section 2: Loading packages and creating a function --#
-# This section only needs to be edited if one of the three packages is not installed already.
+#-- Section 2: Loading shapefile data --#
+# This section uses the input filepath from section 1 and should not need to be edited.
 
-#-- Section 3: Loading shapefile data --#
-# This section uses the input filepath from section 2 and should not need to be edited.
+#-- Section 3: Building a design object --#
+# This section does not need to be edited.
 
-#-- Section 4: Building a design object --#
-# The section needs to be edited to reflect the input and output filepaths.
-
-#-- Section 5: Executing the sample draw and writing it out --#
-# This section uses the output filepath from section 2 and should not need to be edited.
+#-- Section 4: Executing the sample draw and writing it out --#
+# This section uses the output filepath from section 1 and should not need to be edited.
 #------------------------------------------------------------------------------------#
 ######################################################################################
 ######################################
@@ -32,3 +35,10 @@
 # This is a spatially balanced sample draw using the generalized random tessellation stratified approach as implemented in
 # the R package spsurvey::.
 
+# The path to the folder containing the sample frame/stratification polygon shapefile named "sample_frame.shp" is:
+input.filepath <- "C:/Local/filepath/to/data"
+# The path to the folder to write out the resulting design into is:
+output.filepath <- "C:/Local/filepath/to/data"
+
+# The projection that the points should be returned in. NAD83 is the default for all Bureau of Land Management data
+projection <- sp::CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
--- draw_pt5.R
+++ draw_pt5.R
@@ -1,17 +1,46 @@
 
 ######################################
-#### 5: Executing the sample draw and writing it out
+#### 4: Executing the sample draw and writing it out
 ######################################
 
 ## This creates a Spatial Points Data Frame called sample.sites containing the sample draw
 ## It uses the function made in section 1, which calls spsurvey::grts() to draw the points,
 ## then renames the points to fit with AIM expectations
-sample.sites <- grts.custom(design.object = design.object,
-                            design.name = project.name,
-                            sp.object = sample.frame,
-                            stratum.field = "STRATUM",
-                            seed.number = seed.number
-)
+set.seed(seed.number)
+
+# Use spsurvey::grts() first to generate the points
+sample.sites <- spsurvey::grts(design = design.object,
+                               DesignID = project.name,
+                               type.frame = "area",
+                               src.frame = "sp.object",
+                               sp.object = sample.frame,
+                               in.shape = NULL,
+                               stratum = "STRATUM",
+                               shapefile = FALSE)
+
+## Assign projection info to the sample sites from the frame used to create them
+sp::proj4string(sample.sites) <- sample.frame@proj4string
+
+## Reproject the sample sites to whatever the output projection is supposed to be
+sample.sites <- sp::spTransform(sample.sites, projection)
+
+## Update the X and Y coordinate values
+sample.sites$xcoord <- sp::coordinates(sample.sites)[, 1]
+sample.sites$ycoord <- sp::coordinates(sample.sites)[, 2]
+
+## Dropping any extra fields and renaming the remaining
+fields.relevant <- c("siteID", "stratum", "panel", "wgt", "xcoord", "ycoord")
+sample.sites@data <- sample.sites@data[, fields.relevant]
+names(sample.sites@data) <- c("PLOTID", "STRATUM", "PANEL", "IntPtWt", "xcoord", "ycoord")
+
+## Set the panel names to "Oversample" instead of "OverSamp"
+sample.sites@data[sample.sites@data[["PANEL"]] == "OverSamp", "PANEL"] <- "Oversample"
+
+## Rename the plots with the strata
+## Note that this does not attempt to abbreviate
+sample.sites@data$PLOTID <- paste0(sample.sites@data$STRATUM,
+                                   stringr::str_extract(string = sample.sites@data$PLOTID,
+                                                        pattern = "-\\d{1,4}$"))
 
 ## This creates a shapefile called "sample_draw.shp" in the filepath in output.filepath.
 ## If there is already a sample_draw.shp in that filepath, it will be overwritten
