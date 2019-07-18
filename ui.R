70b4f7eff1a595eb05cc32f0ef210883a21bad2c
nstauffer <ngstauffer@gmail.com>
2019-07-18 15:58:22 -0700

    Add comments

--- ui.R
+++ ui.R
@@ -59,10 +59,12 @@ fluidPage(
       conditionalPanel(condition = "input.strataname != ''",
                        actionButton(inputId = "submitstratum",
                                     label = "Update stratum information")),
+      # The empty string is just to take up space and buffer the buttons
       helpText(""),
       conditionalPanel(condition = "input.allocated > 0",
                        actionButton(inputId = "fetch",
                                     label = "Fetch points!")),
+      # The empty string is just to take up space and buffer the buttons
       helpText(""),
       conditionalPanel(condition = "input.fetch > 0",
                        downloadButton(outputId = 'downloadData',
