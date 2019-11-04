3f1103eb5918f7fe33dc2250990bb5a76d425cdb
nstauffer <ngstauffer@gmail.com>
2019-11-04 11:30:04 -0800

    Update message/warning/error pulse color

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ library(shiny)
 
 fluidPage(
   # This changes the position and styling of the notification
+  # Old RGB: 204,169,44
   tags$head(
     tags$style(
       HTML(
@@ -12,32 +13,32 @@ fluidPage(
           width: calc(25%);
           opacity: 1;
           font-weight: bold;
-          box-shadow: 0 0 0 rgba(204,169,44, 0.4);
+          box-shadow: 0 0 0 rgba(181,181,181, 0.4);
           animation: pulse 2s infinite;
         }
         @-webkit-keyframes pulse {
           0% {
-            -webkit-box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+            -webkit-box-shadow: 0 0 0 0 rgba(181,181,181, 0.4);
           }
           70% {
-            -webkit-box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+            -webkit-box-shadow: 0 0 0 10px rgba(181,181,181, 0);
           }
           100% {
-            -webkit-box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+            -webkit-box-shadow: 0 0 0 0 rgba(181,181,181, 0);
           }
         }
         @keyframes pulse {
           0% {
-            -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
-            box-shadow: 0 0 0 0 rgba(204,169,44, 0.4);
+            -moz-box-shadow: 0 0 0 0 rgba(181,181,181, 0.4);
+            box-shadow: 0 0 0 0 rgba(181,181,181, 0.4);
           }
           70% {
-            -moz-box-shadow: 0 0 0 10px rgba(204,169,44, 0);
-            box-shadow: 0 0 0 10px rgba(204,169,44, 0);
+            -moz-box-shadow: 0 0 0 10px rgba(181,181,181, 0);
+            box-shadow: 0 0 0 10px rgba(181,181,181, 0);
           }
           100% {
-            -moz-box-shadow: 0 0 0 0 rgba(204,169,44, 0);
-            box-shadow: 0 0 0 0 rgba(204,169,44, 0);
+            -moz-box-shadow: 0 0 0 0 rgba(181,181,181, 0);
+            box-shadow: 0 0 0 0 rgba(181,181,181, 0);
           }
         }"
       )
