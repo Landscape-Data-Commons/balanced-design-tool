621bc219e6b9eec76250b7e43bab6207ecd41d14
Nelson Stauffer <ngstauffer@gmail.com>
2022-05-03 10:57:30 -0700

    Update to use modern spsurvey

--- draw_pt5.R
+++ draw_pt5.R
@@ -2,50 +2,91 @@
 ######################################
 #### 4: Executing the sample draw and writing it out
 ######################################
+## Since this script was originally written, the function spsurvey::grts() has been
+## overhauled. Parts of this script (like the following chunk) are bridging the
+## old code and the new code.
+
+## This converts the old style design object into the appropriate inputs for spsurvey::grts()
+## The design object will be used again later for redefining panels
+base_counts <- sapply(X = design.object,
+                      FUN = function(X){
+                        sum(X[["panel"]])
+                      })
+
+over_counts <- sapply(X = design.object,
+                      FUN = function(X){
+                        sum(X[["over"]])
+                      })
+
 
 ## This creates a Spatial Points Data Frame called sample.sites containing the sample draw
 ## It uses the function made in section 1, which calls spsurvey::grts() to draw the points,
 ## then renames the points to fit with AIM expectations
 set.seed(seed.number)
 
-# Use spsurvey::grts() first to generate the points
-sample.sites <- spsurvey::grts(design = design.object,
-                               DesignID = project.name,
-                               type.frame = "area",
-                               src.frame = "sp.object",
-                               sp.object = sample.frame,
-                               in.shape = NULL,
-                               stratum = "STRATUM",
-                               shapefile = FALSE)
+## Use spsurvey::grts() first to generate the points
+grts_output <- spsurvey::grts(sframe = sample.frame,
+                              n_base = base_counts,
+                              stratum_var = "STRATUM",
+                              seltype = "equal",
+                              n_over = over_counts)
 
-## Assign projection info to the sample sites from the frame used to create them
-sp::proj4string(sample.sites) <- sample.frame@proj4string
+## For each stratum, we'll do some point cleanup and combination
+points_list <- lapply(X = names(base_counts),
+                      design_object = design.object,
+                      grts_output = grts_output,
+                      FUN = function(X, design_object, grts_output){
+                        # just for convenience and clarity
+                        current_stratum <- X
+                        
+                        # Grab the base points
+                        current_base_points <- grts_output$sites_base[grts_output$sites_base$stratum == current_stratum, ]
+                        # grts() no longer supports panels out of the box,
+                        # so we'll grab the info from the design object and make a vector
+                        panel_info <- design_object[[current_stratum]]$panel
+                        panels <- as.vector(sapply(X = names(panel_info),
+                                                   panel_info = panel_info,
+                                                   FUN = function(X, panel_info){
+                                                     rep(x = X,
+                                                         times = panel_info[X])
+                                                   }))
+                        current_base_points$siteuse <- panels
+                        
+                        current_over_points <- grts_output$sites_over[grts_output$sites_over$stratum == current_stratum, ]
+                        current_over_points$siteuse <- "Oversample"
+                        
+                        # Combine the base and oversample points into a single sf object
+                        current_points <- rbind(current_base_points,
+                                                current_over_points)
+                        
+                        # Trim that sf object down to just the relevant variables
+                        vars_to_keep <- c("siteID", "stratum", "siteuse", "wgt", "lon_WGS84", "lat_WGS84")
+                        
+                        current_points <- current_points[, vars_to_keep]
+                        
+                        # Rename the variables to be in keeping with AIM terminology
+                        names(current_points)[names(current_points) == "siteID"] <- "PLOTID"
+                        names(current_points)[names(current_points) == "stratum"] <- "STRATUM"
+                        names(current_points)[names(current_points) == "siteuse"] <- "PANEL"
+                        names(current_points)[names(current_points) == "wgt"] <- "IntPtWt"
+                        
+                        # Change plot IDs to use the strata names
+                        current_points$PLOTID <- gsub(x = current_points$PLOTID,
+                                                      pattern = "Site",
+                                                      replacement = current_stratum)
+                        
+                        current_points
+                      })
 
-## Reproject the sample sites to whatever the output projection is supposed to be
-sample.sites <- sp::spTransform(sample.sites, projection)
+## Combine all the sf objects into a single sf object
+sample.sites <- do.call(rbind,
+                        points_list)
 
-## Update the X and Y coordinate values
-sample.sites$xcoord <- sp::coordinates(sample.sites)[, 1]
-sample.sites$ycoord <- sp::coordinates(sample.sites)[, 2]
-
-## Dropping any extra fields and renaming the remaining
-fields.relevant <- c("siteID", "stratum", "panel", "wgt", "xcoord", "ycoord")
-sample.sites@data <- sample.sites@data[, fields.relevant]
-names(sample.sites@data) <- c("PLOTID", "STRATUM", "PANEL", "IntPtWt", "xcoord", "ycoord")
-
-## Set the panel names to "Oversample" instead of "OverSamp"
-sample_sites@data[["PANEL"]][sample_sites@data[["PANEL"]] == "OverSamp"] <- "Oversample"
-
-## Rename the plots with the strata
-## Note that this does not attempt to abbreviate
-sample.sites@data$PLOTID <- paste0(sample.sites@data$STRATUM,
-                                   stringr::str_extract(string = sample.sites@data$PLOTID,
-                                                        pattern = "-\\d{1,4}$"))
 
 ## This creates a shapefile called "sample_draw.shp" in the filepath in output.filepath.
 ## If there is already a sample_draw.shp in that filepath, it will be overwritten
-rgdal::writeOGR(obj = sample.sites,
-                dsn = output.filepath,
-                layer = 'sample_draw',
-                driver = 'ESRI Shapefile',
-                overwrite_layer = TRUE)
+sf::st_write(obj = sample.sites,
+             dsn = output.filepath,
+             layer = 'sample_draw',
+             driver = 'ESRI Shapefile',
+             append = FALSE)
